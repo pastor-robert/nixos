4a21a08c0c31d8dbe2e1172fda46f292cfcde9aa
Rob Adams <rob@rob-adams.us>
2026-02-02 11:51:20 -0600

    nix-index-database

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
   inputs = {
     # Official
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    # for command_not_found_handler
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nix-index-database, ... }@inputs: {
     nixosConfigurations.lonsdaleite = nixpkgs.lib.nixosSystem {
       modules = [
         # Import existing config so this version is a NOP
         ./configuration.nix
+
+        # for command_not_found_handler, and nix-locate
+        nix-index-database.nixosModules.default
+
+        # wrap and install comma
+        { programs.nix-index-database.comma.enable = true; }
       ];
     };
   };
